1952362d8410b97faab397747e1025f4b8e543c5
Ibrahim khan <ibrahim2013khan@gmail.com>
2024-05-25 23:55:11 +0600

    Country-App-Project

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,36 @@ const App = () =>  {
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
   const [countries, setCountries] = useState([]);
+
+  const fetchData = async (url) => {
+    setIsLoading(true);
+
+    try{
+      const response = await fetch(url);
+      const data = await response.json();
+      setCountries(data);
+      setIsLoading(false);
+      setError(null);
+    }catch(error){
+      setIsLoading(false);
+      setError(error);
+    }
+
+  }
+
+
+  useEffect (() => {
+    fetchData(url);
+  }, []);
   
 
   return (
     <>
-      <div>
-        
-      </div>
       <h1>Country App</h1>
-      <div className="card">
-       
-        
-      </div>
-      
+      {isLoading && <h2>Loading ...</h2>}
+      {error &&  <h2>{error.message}</h2>}        
     </>
-  )
-}
+  );
+};
 
 export default App
