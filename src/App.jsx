72d6e10f1a431c2fcf3c465830727100981f4543
Ibrahim khan <ibrahim2013khan@gmail.com>
2024-05-31 03:03:04 +0600

    Country App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import './App.css'
 import Countries from './Components/Countries';
+import Search from './Components/Search';
 
 
 const url = "https://restcountries.com/v3.1/all"
@@ -9,7 +10,7 @@ const App = () =>  {
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
   const [countries, setCountries] = useState([]);
-  const [FilteredCountries, setFilteredCountries] = 
+  const [filteredCountries, setFilteredCountries] = 
   useState(countries);
 
 
@@ -37,17 +38,27 @@ const App = () =>  {
   }, []);
 
   const handleRemoveCountry = (name) =>{
-    const filter = filteredCountries.filter((country)
-  => country.name.common)
-  }
+    const filter = filteredCountries.filter((country) => country.name.common !== name);
+    setFilteredCountries(filter)
+  };
   
+  const handleSearch = (searchValue) => {
+    let value = searchValue.toLowerCase();
+    const newCountries = countries.filter((country) => {
+    const countryName = country.name.common.toLowerCase();
+    return countryName.startsWith(value)
+  });
+  setFilteredCountries(newCountries); 
+};
 
   return (
     <>
       <h1>Country App</h1>
+      <Search onSearch={handleSearch}/>
       {isLoading && <h2>Loading ...</h2>}
       {error &&  <h2>{error.message}</h2>}  
-      {countries && <Countries countries={countries} 
+      {countries && <Countries 
+      countries={filteredCountries} 
       onRemoveCountry = {handleRemoveCountry} />}      
     </>
   );
